1e6e8facf8088bdfcbf4c1d5ea1eb6cf538f605b
amalgichevdesign <amalgichev12@gmail.com>
2026-04-13 23:07:55 -0400

    Form Validation Back-End Fix

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import nodemailer from 'nodemailer';
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Nodemailer Setup
 const transporter = nodemailer.createTransport({
