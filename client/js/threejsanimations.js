3e8f2a299bd83a5aef3f70fd5f861636ede9dae5
amalgichevdesign <amalgichev12@gmail.com>
2026-06-18 01:20:13 -0400

    Minor Timing Changes #2

--- client/js/threejsanimations.js
+++ client/js/threejsanimations.js
@@ -911,11 +911,11 @@ const Scene = async(sceneId) => {
 
                         // Rotate the stars in starsSecondUse and constantly move the texture on them.
                         starsSecondUse.forEach((star, idx) => {
-                            star.starMovingTexture.offset.x += 0.0001;
+                            star.starMovingTexture.offset.x += 0.001;
                             if (idx >= starsSecondUse.length / 2) {
-                                star.mesh.rotation.z -= 0.0003;
+                                star.mesh.rotation.z -= 0.003;
                             } else {
-                                star.mesh.rotation.z += 0.0003;
+                                star.mesh.rotation.z += 0.003;
                             };
                         });
 
@@ -1023,11 +1023,11 @@ const Scene = async(sceneId) => {
 
                         switch(idx) {
                             case 0:
-                                capsulesSecondScreen[idx].mesh.rotation.y -= 0.0003;
+                                capsulesSecondScreen[idx].mesh.rotation.y -= 0.003;
 
                                 break;
                             case 1:
-                                capsulesSecondScreen[idx].mesh.rotation.y += 0.0003;
+                                capsulesSecondScreen[idx].mesh.rotation.y += 0.003;
 
                                 break;
                         };
@@ -1039,25 +1039,25 @@ const Scene = async(sceneId) => {
 
                         switch(idx) {
                             case 0:
-                                capsuleBallsSecondScreen[idx].mesh.rotation.y -= 0.0003;
-                                capsuleBallsSecondScreen[idx].mesh.rotation.z -= 0.0003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.y -= 0.003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.z -= 0.003;
 
                                 break;
                             case 1:
-                                capsuleBallsSecondScreen[idx].mesh.rotation.y += 0.0003;
-                                capsuleBallsSecondScreen[idx].mesh.rotation.z += 0.0003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.y += 0.003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.z += 0.003;
 
                                 break;
                             case 2:
-                                capsuleBallsSecondScreen[idx].mesh.rotation.x -= 0.0003;
-                                capsuleBallsSecondScreen[idx].mesh.rotation.y -= 0.0003;
-                                capsuleBallsSecondScreen[idx].mesh.rotation.z -= 0.0003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.x -= 0.003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.y -= 0.003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.z -= 0.003;
 
                                 break;
                             case 3:
-                                capsuleBallsSecondScreen[idx].mesh.rotation.x += 0.0003;
-                                capsuleBallsSecondScreen[idx].mesh.rotation.y += 0.0003;
-                                capsuleBallsSecondScreen[idx].mesh.rotation.z += 0.0003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.x += 0.003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.y += 0.003;
+                                capsuleBallsSecondScreen[idx].mesh.rotation.z += 0.003;
 
                                 break;
                         };
@@ -1353,7 +1353,7 @@ const Scene = async(sceneId) => {
 
                 
                 // Capsule rotations.
-                    capsuleThirdScreen.mesh.rotation.y -= 0.0003;
+                    capsuleThirdScreen.mesh.rotation.y -= 0.003;
                 console.log(renderer.info.memory);
                 
                 
